4fbdb012f0ee4fe7ff6c7eb4582486e25f9995aa
vladzur <vladzur@gmail.com>
2024-08-29 19:53:31 -0400

    Add news section and home changes

--- src/components/BigBanner.vue
+++ src/components/BigBanner.vue
@@ -1,8 +1,9 @@
 <template>
-    <v-parallax src="/img/kai-shodan.jpg" height="800">
+    <v-parallax src="/img/kai-shodan.jpg" height="800"
+        gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)">
         <div class="d-flex flex-column fill-height justify-center align-center text-white">
             <img src="@/assets/logo_kaishodan.png" alt="Kai-Shodan" class="logo">
-            <h1 class="text-h1">
+            <h1 class="text-h1 text-yellow-darken-2">
                 Kai-Shodan
             </h1>
             <h2 class="text-h2">
--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -2,5 +2,6 @@
     <v-app-bar :elevation="1" color="default">
         <v-btn to="/">Inicio</v-btn>
         <v-btn to="/about">Quienes somos</v-btn>
+        <v-btn to="/news">Noticias</v-btn>
     </v-app-bar>
 </template>
